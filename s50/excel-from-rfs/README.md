1d5269fb195e53e59006b21d2ac44d4054edf6a0
Adrián Molnár <adrian.molnar@mobilengine.com>
2023-06-06 11:26:15 +0200

    Excel and QrCode problem done

--- s50/excel-from-rfs/README.md
+++ s50/excel-from-rfs/README.md
@@ -1,10 +1,9 @@
 Excel file olvasás rfs-ből feladat
 ========================
 
-1. Csinálj egy ts scriptet ami a mellékelt form beküldésekor lefut
-    - A script nyissa meg a formon feltöltött xlsx file-t.
-    - Olvassa ki belőle az oldalak neveit, oszlopai, sorai számát és tárolja el ezeket azonosítóval ellátva a Sheets nevű refemben.
-    - Olvassa ki belőle az oldalak tartalmát és tárolja el azokat a hozzá tartozó oldal azonosítójával Data nevű refemben.
-    - Ellenörizd munkád a formon.
-2. Töltsd fel a csatolt xlsx file-t a csatolt formon.
-3. Ha mindent jól csináltál frisítés után megjelenik a formon az előzéleg feltöltött xlsx-file tartalma.
+1. Csinálj egy ts scriptet ami a mellékelt upload form beküldésekor lefut
+    - A script nyissa meg a formon feltöltött <b>alma</b> xlsx file-t.
+    - Olvassa ki belőle az oldalak neveit, oszlopai, sorai számát és tárolja el ezeket azonosítóval ellátva a <b>tabs</b> nevű refemben.
+    - Olvassa ki belőle az oldalak tartalmát és tárolja el azokat a hozzá tartozó oldal azonosítójával <b>exelData</b> nevű refemben.
+2. Töltsd fel a csatolt <b>alma</b> xlsx file-t az update formon.
+3. Ha mindent jól csináltál megjelenik a <b>table</b> formon az előzéleg feltöltött xlsx-file tartalma.
--- s50/excel-from-rfs/solution/uploadData.ts
+++ s50/excel-from-rfs/solution/uploadData.ts
@@ -1,8 +1,8 @@
 //#server typescript program storeExcelContent for form upload
 //#using reftab Tabs;
-//#using reftab Gyumolcs;
+//#using reftab exelData;
 {
-    var i = 0;
+    var i = db.Tabs.Read({}).Count();
     for (let file of form.uploadXlsx.files) {
         let xlsxfile = fileref.New(file.mediaId, 0);
         let workbook = excel.FromFileref(xlsxfile);
@@ -10,31 +10,23 @@
         for (let sheet of sheets) {
             let colCount = workbook.ColumnCount(sheet)
             let rowCount = workbook.RowCount(sheet)
-            db.Tabs.Insert({tab_id: i, tab_name: sheet, tab_col_num: rowCount, tab_row_num: colCount});
-            for (let r = 0; r < rowCount; r++) {
+            db.Tabs.Insert({tab_id: i, tab_name: sheet+i, tab_col_num: rowCount, tab_row_num: colCount});
+            for (let r = 1; r < rowCount; r++) {
                     let id = i;
                     let nev = workbook.GetValue(sheet, r, 0);
                     let tipus = workbook.GetValue(sheet, r, 1);
                     let ar = workbook.GetValue(sheet, r, 2);
                     let suly = workbook.GetValue(sheet, r, 3);
                     let szallitas = workbook.GetValue(sheet, r, 4);
-
+                    
                     let parsedint = int.Parse(ar);
-                    let parsedfloat = float.Parse(suly);
-
-                    //let exeldtdb = excel.ExcelDateToDtdb(szallitas) ;
-                    let exeldtdb = dtl.Now().DtlToDtdb()
-                    Log("id: "+id);
-                    Log("gy_nev: "+nev);
-                    Log("gy_tipus: "+tipus);
-                    Log("gy_ar: "+parsedint);
-                    Log("gy_suly: "+parsedfloat);
-                    Log("gy_szalitas: "+exeldtdb);
-                    if(r!=0){
-                        db.Gyumolcs.Insert({gy_id: id, gy_nev: nev, gy_tipus: tipus, gy_ar: parsedint, gy_suly: parsedfloat})
+                    if(nev != null && tipus != null){
+                        Log("id:" +id+"nev: "+ nev +", tipus: "+tipus+", ar:  "+ ar +", suly:  "+ suly+", szallit:  "+ szallitas)
+                        db.exelData.Insert({gy_id: id, gy_nev: nev, gy_tipus: tipus, gy_ar: parsedint, gy_suly: suly, gy_szallitas: szallitas})
                     }
-            }
+                }
             i = i+1;
+            Log("Done "+i)
         }
 	}
 }
